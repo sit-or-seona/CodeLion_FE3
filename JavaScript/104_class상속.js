8d9ecc0567374c1f88b3009092612e74c1a2a4a9
seona.bang <sit.or.seona@gmail.com>
2022-10-25 22:39:07 +0900

    221025 JavaScript

--- JavaScript/104_class상속.js
+++ JavaScript/104_class상속.js
@@ -27,3 +27,28 @@ class BabyRobot extends Robot {
 }
 
 const babyRobot = new BabyRobot('브랜든');
+
+
+// 실습 : 소시지를 만드는 클래스를 정의해봅시다.
+// 1. 생성자 함수는 두 가지 매개변수를 전달 받을 수 있으며 전달되는 매개변수에 따라 소시지의 맛이 결정됩니다.
+// 2. 소시지 객체는 taste 라는 메서드가 존재합니다. 생성자함수에서 전달받은 재료에 따라 맛을 나타내는 콘솔로그를 출력하는 역할을 합니다. (예시 : ‘소고기’ 와 ‘파’ 를 매개변수로 전달하였을 경우, ‘소고기와 파 맛이 난다!’ 는 콘솔 메세지를 출력합니다.)
+// 3. 소시지 클래스를 상속받는 FiresSausage 파생클래스를 생성해봅니다. 파생클래스의 taste 메서드를 실행하면 콘솔 메세지에 불맛이 나기 시작합니다.
+
+class Sausage {
+    constructor(flavor1, flavor2) {
+        this.flavor1 = flavor1;
+        this.flavor2 = flavor2;
+    }
+
+    taste() {
+        console.log(`${this.flavor1}와 ${this.flavor2} 맛이 난다!`);
+    }
+}
+
+class FiresSausage extends Sausage {
+    taste() {
+        console.log(`🔥 ${this.flavor1}와 ${this.flavor2} 맛이 난다!🔥`);
+    }
+}
+
+const firesSausage = new FiresSausage('콘옥수수', '소고기');
