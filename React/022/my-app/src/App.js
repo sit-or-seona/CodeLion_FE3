4c20935d45234f8fdb5b0648928199b287f80dba
seona.bang <sit.or.seona@gmail.com>
2022-11-10 17:33:20 +0900

    221110 React

--- React/022/my-app/src/App.js
+++ React/022/my-app/src/App.js
@@ -1,9 +1,13 @@
+import Main from "./components/Main";
+import Modal from "./components/Modal";
+
 function App() {
   return (
     <>
-      
+      <Main/>
+      <Modal/>
     </>
   );
 }
 
-export default App;
+export default App
