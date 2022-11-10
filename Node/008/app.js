28cfb1de0ec0c8a69a202b0f6db3f7bd170f111a
seona.bang <sit.or.seona@gmail.com>
2022-11-10 10:33:42 +0900

    221110 Node

---
+++ Node/008/app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer(function(req, res){
+    console.log('server가 구동중입니다.');
+    console.log(req.headers);
+    console.log(req.method);
+    console.log(req.url);
+    res.write('<h1 style="color:black">hello world</h1>');
+    // 다크모드 시에는 검은색 배경이 됩니다.
+    res.end();
+});
+
+server.listen(8080);
