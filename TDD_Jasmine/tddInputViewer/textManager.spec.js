0d41e764d9b92674269f8685bff97e692b3d45ff
seona.bang <sit.or.seona@gmail.com>
2022-11-30 18:53:04 +0900

    221130 TDD

---
+++ TDD_Jasmine/tddInputViewer/textManager.spec.js
@@ -0,0 +1,16 @@
+describe('텍스트 매니저 테스트입니다.', () => {
+    const textManager = new TextManager();
+
+    it('텍스트 값을 전달합니다.', () => {
+        const initValue = textManager.getValue();
+
+        expect(textManager.getValue()).toBe(initValue);
+    })
+
+    it('텍스트 값을 수정합니다.', () => {
+        const textValue = { data: "Hello Zebras!"}
+        textManager.setValue(textValue);
+
+        expect(textManager.getValue()).toBe(textValue.data);
+    })
+})
